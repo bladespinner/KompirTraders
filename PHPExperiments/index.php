59ba28d93de567a82df8fadc0b9d9ca7ed929d96
Mihail Gospodinov <bladespinner@gmail.com>
2013-04-09 18:49:11 +0300

    Added PHPExperiments folder and almost completed potatosource

--- PHPExperiments/index.php
+++ PHPExperiments/index.php
@@ -5,12 +5,31 @@
 		<script src="Resources/jquery-1.9.1.min.js"></script>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	</head>
-
-	<body>
+	<div>
+	 <form class="user">
+			<input type="text" value="Username" onfocus="if (this.value=='Username') this.value='';"/>
+		</br>
+			<input type="password" value="********" onfocus="if (this.value=='********') this.value='';"/>
+		</br>
+			<input type="submit" value="Login :)"/>
+	</form>
+	</div>
+		<body>
+		<?php 
+		session_start();
+		$count=1;
+		if(isset($_SESSION["count"]))
+			$_SESSION["count"]++;
+		else
+		{
+			$_SESSION["count"]=1;
+		}
+		$count=$_SESSION["count"];
+		echo "<h5> You have visited this site </br> " .$count. "times.</h5>";
+		?> 
 		<div class="wrapper">
 			<img src="Resources/kompir.png" class="icon"></img>
 			<div class="cutted">
-			
 				<header>
 					<h1>
 						Kompir<span>Traders</span>
@@ -19,15 +38,15 @@
 				<content>
 					<aside>
 						<div class="buttons">
-							<a href="index.html">Home</a>
+							<a href="index.php">Home</a>
 							<h2>Potatoes by:</h2>
-							<a href="potato.html?page=0">Variety</a>
-							<a href="index.html">Source</a>
-							<a href="index.html">Popularity</a>
+							<a href="potato.html">Variety</a>
+							<a href="index.php">Source</a>
+							<a href="index.php">Popularity</a>
 							<h2>More:</h2>
 							<a href="about.html">About</a>
-							<a href="index.html">Subscriptions</a>
-							<a href="index.html">Search</a>
+							<a href="index.php">Subscriptions</a>
+
 						</div>
 					</aside>
 					<div class="main">
